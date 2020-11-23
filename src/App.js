72b081bbc1d9ef8992af73064c126d2495825640
Angie Rodriguez <angrodri902@gmail.com>
2020-11-23 13:22:12 -0800

    Install and set up firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import EmailForm from "./EmailForm";
 // import Search from "./Search";
 
 function App() {
   return (
     <div className="App">
       <h1>Firebase Practice</h1>
-      <button>Sign In</button>
+      <EmailForm />
     </div>
   );
 }
